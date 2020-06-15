2949a053e66955c36255beb07c7b4160489f14d8
Pavel Khatyliou <hatylevpavel@gmail.com>
2020-06-15 19:11:41 +0300

    init: start react app

---
+++ rslang/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import MainPage from './MainPage/MainPage';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SUPER FANCY RSLANG APP</h1>
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ rslang/src/MainPage/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div>
+            <h2>
+                There is no main page
+            </h2>
+            <p>Sorry</p>
+        </div>
+    )
+}
+
+export default MainPage
